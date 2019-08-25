3c57cab8bf86b25210710c4c70fb10c827589e38
Luca13-cmyk <52060219+Luca13-cmyk@users.noreply.github.com>
2019-08-25 16:28:43 -0300

    Update cpu_test.py

--- cpu_test.py
+++ cpu_test.py
@@ -28,26 +28,54 @@ import _thread
     
     
 
-try:
-    def fig_gen(max):
-       a, b, contador = 0, 1, 0
-       while contador < max:
-           a, b = b, a + b
-           yield a
-           contador += 1
-       inicio = time.time()
-       for n in fig_gen(strs):
-           print("{0} |".format(n))
-       fim = time.time() - inicio
-       if fim:
-           print("O processador levou {0} segundos".format(fim))
-    strs = int(sys.argv[1])
-    _thread.start_new_thread( fig_gen, (strs) )
-except IndexError:
-    print("EX: cpu_test 100000")
-except:
-    print("Thread err")
+# try:
+#     def fig_gen(max):
+#        a, b, contador = 0, 1, 0
+#        while contador < max:
+#            a, b = b, a + b
+#            yield a
+#            contador += 1
+#        inicio = time.time()
+#        for n in fig_gen(strs):
+#            print("{0} |".format(n))
+#        fim = time.time() - inicio
+#        if fim:
+#            print("O processador levou {0} segundos".format(fim))
+#     strs = int(sys.argv[1])
+#     _thread.start_new_thread( fig_gen, (strs) )
+# except IndexError:
+#     print("EX: cpu_test 100000")
+# except:
+#     print("Thread err")
 
+
+
+# import sys
+# import time
+# import threading
+
+# try:
+#     strs = int(sys.argv[1])
+    
+#     def fig_gen(max):
+#         a, b, contador = 0, 1, 0
+#         while contador < max:
+#             a, b = b, a + b
+#             yield a
+#             contador += 1
+    
+#     def lo(par):
+#         inicio = time.time()
+#         for n in fig_gen(par):
+#             print("{0} |".format(n))
+#         fim = time.time() - inicio
+#         if fim:
+#             print("O processador levou {0} segundos".format(fim))
+# #     t = threading.Thread(target=lo, args=(strs,))
+# #     t.start()
+#     lo(strs)  
+# except:
+#     print("erro")
             
 
 
